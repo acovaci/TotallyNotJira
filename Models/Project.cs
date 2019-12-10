6033dca19057c3b37f45d4a6d9ddae96502c67f3
AdiCovaci <adrianclaudiucovaci@gmail.com>
2019-12-10 17:09:08 +0000

    Fixed typo
    Signed-off-by: AdiCovaci <adrianclaudiucovaci@gmail.com>

---
+++ Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TotallyNotJira.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ProjectDBContext : DbContext
+    {
+        public ProjectDBContext() : base("DBConnectionString") { }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
