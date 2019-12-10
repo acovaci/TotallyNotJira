980455927a1dd23d0c510c9207535796abefd444
AdiCovaci <adrianclaudiucovaci@gmail.com>
2019-12-10 17:09:08 +0000

    Added Project Model with Index and New
    Signed-off-by: AdiCovaci <adrianclaudiucovaci@gmail.com>

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TotallyNotJira.Models;
+
+namespace TotallyNotJira.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private ProjectDBContext db = new ProjectDBContext();
+        // GET: Project
+        public ActionResult Index()
+        {
+            ViewBag.Projects = db.Projects.OrderBy(x => x.Name);
+
+            return View();
+        }
+
+        // GET: Project/Details/5
+        public ActionResult Details(int id)
+        {
+            ViewBag.Project = db.Projects.Find(id);
+
+            return View();
+        }
+
+        // GET: Project/Create
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Project/Create
+        [HttpPost]
+        public ActionResult New(Project project)
+        {
+            try
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Project/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Project = db.Projects.Find(id);
+
+            return View();
+        }
+
+        // POST: Project/Edit/5
+        [HttpPut]
+        public ActionResult Edit(int id, Project updatedProject)
+        {
+            try
+            {
+                Project project = db.Projects.Find(id);
+
+                if (TryUpdateModel(project))
+                {
+                    project.Name = updatedProject.Name;
+                    project.Description = updatedProject.Description;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Project/Delete/5
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace TotallyNotJira.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TotallyNotJira.Models.ProjectDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "TotallyNotJira.Models.ProjectDBContext";
+        }
+
+        protected override void Seed(TotallyNotJira.Models.ProjectDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
